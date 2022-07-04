e4ed963d9e1b8c2888413a2c51bc7abbd5956d41
Vincent Petit <vincent.petit@efrei.net>
2022-07-04 17:54:09 +0200

    Added views and a working BarChart component

---
+++ client/src/components/ChartBar.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+	import { h } from 'vue';
+	import { Bar } from 'vue-chartjs';
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		CategoryScale,
+		LinearScale,
+	} from 'chart.js';
+	ChartJS.register(
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		CategoryScale,
+		LinearScale
+	);
+
+	interface Props {
+		chartData: {
+			labels: string[];
+			datasets: object[];
+		};
+		chartOptions: object;
+		chartId: string;
+		datasetId: string;
+		width?: number;
+		height?: number;
+		cssClasses?: string;
+		styles?: object;
+		plugins?: object;
+	}
+	const props = withDefaults(defineProps<Props>(), {
+		chartId: 'bar-chart',
+		datasetId: 'bar-chart-dataset',
+		width: 800,
+		height: 800,
+	});
+
+	// Added a ts-ignore flag because Bar is not recognized by as a valid
+	// HTML element.
+	const barChart = () => {
+		//@ts-ignore:next-line
+		return h(Bar, {
+			chartData: props.chartData,
+			chartOptions: props.chartOptions,
+			chartId: props.chartId,
+			width: props.width,
+			height: props.height,
+			cssClasses: props.cssClasses,
+			styles: props.styles,
+			plugins: props.plugins,
+		});
+	};
+</script>
+<template>
+	<barChart />
+</template>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+	import ChartBar from '../components/ChartBar.vue';
+	import Navbar from '../components/Navbar.vue';
+
+	const chartData = {
+		labels: ['January', 'February', 'March'],
+		datasets: [{ data: [40, 20, 12] }],
+	};
+	const chartOptions = { responsive: false };
+	const height = 800;
+	const width = 800;
+</script>
+<template>
+	<Navbar></Navbar>
+	<ChartBar :chart-data="chartData" :chart-options="chartOptions"></ChartBar>
+</template>
+<style lang="scss"></style>
