89d90b26c45af375caffd3d175820e344be3c29f
Vincent Petit <vincent.petit@efrei.net>
2022-07-06 00:58:53 +0200

    navbar logo update

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -6,7 +6,10 @@
 			class="flex flex-1 bg-gradient-to-b from-primary-300 to-primary-100 p-8"
 		>
 			<div class="flex flex-1 gap-10 justify-start items-center">
-				<img class="w-10" src="../assets/logo.png" alt="" />
+				<div class="flex gap-1 items-center">
+					<img class="w-10" src="../assets/logo.png" alt="" />
+					<p class="text-3xl text-accent">AI Shooter</p>
+				</div>
 				<a href="/"> Home</a>
 				<a href="/about">About</a>
 				<a href="/contact">Contact</a>
