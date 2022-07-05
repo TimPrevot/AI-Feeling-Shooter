7bb6f481158bafa0fced741c0139a7a1ba9216b4
Vincent Petit <vincent.petit@efrei.net>
2022-07-06 00:54:40 +0200

    navbar & home

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-	<header class="container mx auto">
+	<header>
 		<nav
 			class="flex flex-1 bg-gradient-to-b from-primary-300 to-primary-100 p-8"
 		>
-			<div class="flex flex-1 gap-10 justify-start">
+			<div class="flex flex-1 gap-10 justify-start items-center">
 				<img class="w-10" src="../assets/logo.png" alt="" />
-				<a href="/">Home</a>
+				<a href="/"> Home</a>
 				<a href="/about">About</a>
 				<a href="/contact">Contact</a>
 			</div>
