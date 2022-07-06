ad351275d4b26badaf777014da4335a5384e8faf
Vincent Petit <vincent.petit@efrei.net>
2022-07-07 01:28:18 +0200

    navbar logo

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,15 +3,14 @@
 <template>
 	<header>
 		<nav
-			class="flex flex-1 bg-gradient-to-b from-primary-300 to-primary-100 p-8"
+			class="h-28 flex flex-1 gap-10 bg-gradient-to-b from-primary-300 to-primary-100 p-8"
 		>
+			<img src="../assets/logo.png" alt="" />
 			<div class="flex flex-1 gap-10 justify-start items-center">
-				<div class="flex gap-1 items-center">
-					<img class="w-10" src="../assets/logo.png" alt="" />
-				</div>
-				<a href="/"> Home</a>
-				<a href="/about">About</a>
-				<a href="/contact">Contact</a>
+				<router-link to="/"> Home </router-link>
+				<router-link to="/products"> Products </router-link>
+				<router-link to="/register"> Register </router-link>
+				<router-link to="/login"> Login </router-link>
 			</div>
 			<div class="justify-end">
 				<div>
