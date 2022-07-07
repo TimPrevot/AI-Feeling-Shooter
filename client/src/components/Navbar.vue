fd68ce4f1d9478d840bcaa346fdd8741100a1fd4
Vincent Petit <vincent.petit@efrei.net>
2022-07-07 09:28:52 +0200

    navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,23 +1,64 @@
 <script setup lang="ts"></script>
 
 <template>
-	<header>
-		<nav
-			class="h-28 flex flex-1 gap-10 bg-gradient-to-b from-primary-300 to-primary-100 p-8"
-		>
-			<img src="../assets/logo.png" alt="" />
-			<div class="flex flex-1 gap-10 justify-start items-center">
-				<router-link to="/"> Home </router-link>
-				<router-link to="/products"> Products </router-link>
-				<router-link to="/register"> Register </router-link>
-				<router-link to="/login"> Login </router-link>
-			</div>
-			<div class="justify-end">
-				<div>
-					<img class="w-10" src="../assets/logo.png" alt="" />
+	<header class="bg-white w-full flex items-center">
+		<div class="container">
+			<div class="flex mx-2 items-center justify-between relative">
+				<div class="px-4 w-40 max-w-full">
+					<a href="javascript:void(0)" class="w-full block py-5">
+						<img
+							src="../assets/logo.png"
+							alt="logo"
+							class="w-full"
+						/>
+					</a>
+				</div>
+				<div class="flex px-4 justify-between items-center w-full">
+					<div>
+						<nav
+							class="absolute py-5 px-6 lg:static lg:shadow-none"
+						>
+							<ul class="flex gap-6">
+								<li>
+									<router-link
+										to="/home"
+										class="py-3 px-5 flex inline-flex ease-in duration-300 hover:bg-primary-500 rounded-lg"
+									>
+										Home
+									</router-link>
+								</li>
+								<li>
+									<router-link
+										to="/pricing"
+										class="py-3 px-5 flex inline-flex ease-in duration-300 hover:bg-primary-500 rounded-lg"
+									>
+										Pricing
+									</router-link>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<ul class="flex gap-6 justify-end">
+						<li>
+							<router-link
+								to="/login"
+								class="py-3 px-5 ease-in duration-300 hover:bg-primary-500 rounded-lg"
+							>
+								Login
+							</router-link>
+						</li>
+						<li>
+							<router-link
+								to="/register"
+								class="py-3 px-5 bg-primary-300 ease-in duration-100 hover:bg-primary-500 rounded-lg"
+							>
+								Sign Up
+							</router-link>
+						</li>
+					</ul>
 				</div>
 			</div>
-		</nav>
+		</div>
 	</header>
 </template>
 
