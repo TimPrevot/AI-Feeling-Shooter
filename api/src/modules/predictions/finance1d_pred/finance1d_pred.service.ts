d2cab0354e77504dc1249967860e600d2a8670fc
TimPrevotPro <timothe.prevot@efrei.net>
2022-07-08 12:16:01 +0200

    feat: added date in return of finance1d pred route

--- api/src/modules/predictions/finance1d_pred/finance1d_pred.service.ts
+++ api/src/modules/predictions/finance1d_pred/finance1d_pred.service.ts
@@ -21,17 +21,20 @@ export class Finance1d_predService {
     const preds = await this.findAll();
     const close_values = [];
     const predicted_values = [];
+    const dates = [];
     for (let i = 0; i < preds.length; i++) {
       if (isNaN(preds[i].Close) || isNaN(preds[i].Predicted_Close)) {
         continue;
       } else {
         close_values.push(preds[i].Close);
         predicted_values.push(preds[i].Predicted_Close);
+        dates.push(preds[i].Date.toDateString());
       }
     }
     return {
       close_values,
       predicted_values,
+      dates,
     };
   }
 }
